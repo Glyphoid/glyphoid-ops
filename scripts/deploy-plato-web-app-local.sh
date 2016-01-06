89cc5408e6c283a6510412545126954adf7594dd
shanqing.cai <shanqing.cai@gmail.com>
2016-01-06 10:51:59 -0500

    Clean up deploy-plato-web-app-local.sh

--- scripts/deploy-plato-web-app-local.sh
+++ scripts/deploy-plato-web-app-local.sh
@@ -1,30 +1,37 @@
+#!/bin/bash
+
 # Configuration
-echo $#
-echo "Usage"
-if [ $# -eq 0 ]
+if [ $# -ne 2 ]
 then
-    echo "Usage"
+    echo "Usage <RESOURCE_ROOT> <PLATO_ASSET_ROOT>"
+    echo "      E.g., ./deploy-plato-web-app-local.sh /home/shanqing/plato/math-handwriting-lib/src/main/resources /opt/plato-server"
     exit 1
 fi
 
-PLATO_WAR=/C/Users/scai/Dropbox/Plato/PlatoWebApp/target/PlatoWebApp.war
+RESOURCES_ROOT=$1
+echo RESOURCES_ROOT = "${RESOURCES_ROOT}"
+PLATO_ASSET_ROOT=$2
+echo PLATO_ASSEST_ROOT = "${PLATO_ASSET_ROOT}"
+echo
 
-RESOURCES_ROOT_DIR=/C/Users/scai/Dropbox/javaWS/handwriting/src/main/resources
+PRODUCTION_FILE=${RESOURCES_ROOT}/config/productions.txt
+TERMINALS_JSON_FILE=${RESOURCES_ROOT}/config/terminals.json
+#TOKEN_ENGINE_SER_FILE_WHR0=token_engine.sdv.sz0_whr0_ns1.ser
+TOKEN_ENGINE_SER_FILE_WHR1=${RESOURCES_ROOT}/token_engine/token_engine.sdv.sz0_whr1_ns1.ser
+STROKE_CURATOR_CONFIG_JSON_FILE=${RESOURCES_ROOT}/config/stroke_curator_config.json
 
-PRODUCTION_FILE=${RESOURCES_ROOT_DIR}/config/productions.txt
-TERMINALS_JSON_FILE=${RESOURCES_ROOT_DIR}/config/terminals.json
-TOKEN_ENGINE_SER_FILE_WHR0=/C/Users/scai/Dropbox/Plato/engines/token_engine.sdv.sz0_whr0_ns1.ser
-TOKEN_ENGINE_SER_FILE_WHR1=/C/Users/scai/Dropbox/Plato/engines/token_engine.sdv.sz0_whr1_ns1.ser
-STROKE_CURATOR_CONFIG_JSON_FILE=${RESOURCES_ROOT_DIR}/config/stroke_curator_config.json
-PLAO_SECRETS_PROPERTIES_FILE=/C/Users/scai/Dropbox/Plato/secure/plato-secrets-dev.properties
-PLATO_AWS_PROPERTIES_FILE=/C/Users/scai/Dropbox/Plato/secure/plato-aws.properties
+mkdir -p ${PLATO_ASSET_ROOT}/token-set-parser &&
+mkdir -p ${PLATO_ASSET_ROOT}/token-engine &&
+mkdir -p ${PLATO_ASSET_ROOT}/stroke-curator &&
 
-PLATO_SERVER_DIR=/C/plato-server
+cp -v ${PRODUCTION_FILE} ${PLATO_ASSET_ROOT}/token-set-parser/ &&
+cp -v ${TERMINALS_JSON_FILE} ${PLATO_ASSET_ROOT}/token-set-parser/ &&
+#cp -v ${TOKEN_ENGINE_SER_FILE_WHR0} ${PLATO_ASSET_ROOT}/token-engine/ &&
+cp -v ${TOKEN_ENGINE_SER_FILE_WHR1} ${PLATO_ASSET_ROOT}/token-engine/ &&
+cp -v ${STROKE_CURATOR_CONFIG_JSON_FILE} ${PLATO_ASSET_ROOT}/stroke-curator/ &&
+#cp -v ${PLAO_SECRETS_PROPERTIES_FILE} ${PLATO_ASSET_ROOT}/ &&
+#cp -v ${PLATO_AWS_PROPERTIES_FILE} ${PLATO_ASSET_ROOT}/ &&
 
-cp -v ${PRODUCTION_FILE}       ${PLATO_SERVER_DIR}/token-set-parser/
-cp -v ${TERMINALS_JSON_FILE}    ${PLATO_SERVER_DIR}/token-set-parser/
-cp -v ${TOKEN_ENGINE_SER_FILE_WHR0} ${PLATO_SERVER_DIR}/token-engine/
-cp -v ${TOKEN_ENGINE_SER_FILE_WHR1} ${PLATO_SERVER_DIR}/token-engine/
-cp -v ${STROKE_CURATOR_CONFIG_JSON_FILE} ${PLATO_SERVER_DIR}/stroke-curator/
-cp -v ${PLAO_SECRETS_PROPERTIES_FILE} ${PLATO_SERVER_DIR}/
-cp -v ${PLATO_AWS_PROPERTIES_FILE} ${PLATO_SERVER_DIR}/
+echo
+echo "DONE"
+echo "** IMPORTANT **: Do not forget to copy plato-secrets-dev.properties and plato-aws.properties to ${PLATO_ASSET_ROOT}"
